ee613e93172afbb498c144b986c8c3bb7268a90f
Don Ferguson <dferguso@mix.wvu.edu>
2014-07-27 00:52:44 -0400

    Edited chacematrix.R file - R Programmig
    Edited the code in the files in order to calculate the inverse of matrix
    and cache the results.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Lexical Scoping is one form of RULES that define the process for associating 
+## symbols with values.  It states that the values of the free variables are 
+## defined by the bindings that were in effect at the time the function
+## was created
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function closure indicating that it is a function
+## together with its environment
 
-makeCacheMatrix <- function(x = matrix()) {
+## This line indicates that makeCacheMatrix is a function with a single 
+## argument. The argument is a matrix.  The function passes other functions: set,
+## get, setSolve, and getSolve.
 
+makeCacheMatrix <- function(x = matrix()) { # x is a formal parameter
+    m <- NULL  #
+    set <- function(y) { # This function returns x
+        x <<- y
+        m <<- NULL  
+    }
+    get <- function() x  ## Returns the argument "x"
+    setSolve <- function(solve) m <<- solve  #setSolve performs the iteration
+    getSolve <- function() m  # Retrieves values from archive
+    list(set = set, get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
@@ -12,4 +29,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getSolve()
+    if(!is.null(m)) { #if m is not null than get the CACHED values
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setSolve(m)
+    m
 }
